bcc99faa7fe1fe697d1bb7f93c5eabd2eb4b3802
toolateralus <joshnjoshgaming@gmail.com>
2024-05-26 14:17:31 -0400

    Fix logging bug in log_memory function

--- src/functions.rs
+++ src/functions.rs
@@ -5,13 +5,13 @@ use std::io::Write;
 pub fn log_memory(cpu: &mut Cpu) {
     let start_idx = cpu.registers[0] as usize;
     let end_idx = cpu.registers[1] as usize;
-    let range = &cpu.memory.buffer[start_idx..end_idx];
     
-    if let Ok(mut file) = OpenOptions::new().append(true).open("logs.txt") {
-        if let Err(err) = writeln!(file, "memory at {} to {}, {:?}", start_idx, end_idx, range) {
-            eprintln!("Failed to write to file: {}", err);
-        }
-    } else {
-        eprintln!("Failed to open file");
+    let mut file = OpenOptions::new().append(true).open("logs.txt").unwrap();
+    if start_idx > end_idx {
+        writeln!(file, "log memory got invalid start and end index. {} to {}", start_idx, end_idx).unwrap();    
+        return;
     }
+    
+    let range = &cpu.memory.buffer[start_idx..end_idx];
+    writeln!(file, "memory at {} to {}, {:?}", start_idx, end_idx, range).unwrap();
 }
